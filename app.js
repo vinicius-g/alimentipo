5c69f13feb3f155fc8b728ab4be3e513ef65ed3c
Vinícius Gonçalves <vinicius.devlp@gmail.com>
2023-10-09 00:38:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const rotas = require("./app/routes/router");
 
 const app = express();
-const port = process.env.PORRT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(session({
     secret: process.env.SECRET,
